8460090e72e47728443dcc2062a2c6dd1d2f1cb6
Aniket Dongare <aniketd423@gmail.com>
2020-09-21 21:39:12 +0530

    added files

---
+++ bot.py
@@ -0,0 +1,41 @@
+import tweepy
+import time
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+user = api.me()
+
+def limit_handler(cursor):
+    try:
+        while True:
+            yield cursor.next()
+    except tweepy.RateLimitError:
+        time.sleep(1000)
+
+search_string = 'Covid'
+numberOfTweets = 5
+
+for tweet in tweepy.Cursor(api.search, search_string).items(numberOfTweets):
+    try:
+        tweet.favorite()
+        print('I liked that tweet')
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
+
+
+
+
+
+
+
+
+# #Generous Bot
+# for follower in limit_handler(tweepy.Cursor(api.followers).items()):
+#     if follower.name == 'follower_name':
+#       follower.follow()
+#       break
